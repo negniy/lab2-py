b427489b212ca18e9bbda66a05faf1a66f96f801
Dmitrii Stepanov <dstepanov200498@gmail.com>
2022-10-17 15:17:31 +0400

    сделано создание аннтотации

--- get_path.py
+++ get_path.py
@@ -1,22 +1,22 @@
 import os
 
 
-def create_download_relative_way(name_class, number):
+def download_relative_way(name_class, number):
     return f"dataset/download_data/{name_class}/{str(number).zfill(4)}.jpg"
 
 
-def create_changed_relative_way(name_class, number):
+def changed_relative_way(name_class, number):
     return f"dataset/dataset_another/{name_class}_{str(number).zfill(4)}.jpg"
 
 
-def create_random_relative_way(number):
+def random_relative_way(number):
     return f"dataset/dataset_number/{str(number).zfill(4)}.jpg"
 
 
 def get_absolute_way(name_class, number, mode):
     if mode == "download":
-        return os.path.abspath(create_download_relative_way(name_class, number))
+        return os.path.abspath(download_relative_way(name_class, number))
     if mode == "changed":
-        return os.path.abspath(create_changed_relative_way(name_class, number))
+        return os.path.abspath(changed_relative_way(name_class, number))
     if mode == "random":
-        return os.path.abspath(create_random_relative_way(number))
+        return os.path.abspath(random_relative_way(number))
